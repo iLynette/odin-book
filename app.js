2c704b96cee0f81f3e4e75aca9854a782cc415d2
iLynette <lynetteacholah@icloud.com>
2020-11-26 18:49:40 +0300

    changes

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
 const exphbs = require('express-handlebars')
+const methodOverride = require('method-override')
 const passport = require('passport')
 const session = require('express-session')
+const MongoStore = require('connect-mongo')(session)
 const connectDB = require('./config/db')
 
 //load config
@@ -18,25 +20,63 @@ connectDB()
 
 const app = express()
 
+//body parser
+app.use(express.urlencoded({ extended: false}))
+app.use(express.json())
+
+//method override
+app.use(
+  methodOverride(function (req, res) {
+    if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+      // look in urlencoded POST bodies and delete it
+      let method = req.body._method
+      delete req.body._method
+      return method
+    }
+  })
+)
+
 //logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'))
 }
 
+//handlebars helpers
+const { formatDate, stripTags, truncate, editIcon, select} = require('./helpers/hbs')
+
 //handlebars
-app.engine('.hbs', exphbs({ defaultLayout: 'main', extname: '.hbs'}));
+app.engine(
+  '.hbs', 
+  exphbs({ helpers: {
+      formatDate,
+      stripTags,
+      truncate,
+      editIcon,
+      select,
+  },
+  defaultLayout: 'main', 
+  extname: '.hbs',
+})
+)
 app.set('view engine', '.hbs');
 
 //sessions
 app.use(session ({
-  secret: 'keyword cat',
+  secret: 'keyboard cat',
   resave: false,
   saveUninitialized: false,
+  store: new MongoStore({ mongooseConnection: mongoose.connection}),
 }))
 
 //passport middleware
 app.use(passport.initialize())
 app.use(passport.session())
+
+//set global var
+app.use(function(req, res, next) {
+  res.locals.user = req.user || null
+  next()
+})
 
 //static folder
 app.use(express.static(path.join(__dirname, 'public')))
@@ -44,6 +84,8 @@ app.use(express.static(path.join(__dirname, 'public')))
 //routes
 app.use('/', require('./routes/index'))
 app.use('/auth', require('./routes/auth'))
+app.use('/stories', require('./routes/stories'))
+
 
 const PORT = process.env.PORT || 3000
 
